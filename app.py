800c331f988814b18ae9f9cea466db4e1f4c8215
Valonso <80985309+valonsogit@users.noreply.github.com>
2023-10-10 21:18:21 +0200

    perf/optimize-flask-app
    Saves around 4 bytes of traffic when calling the main route by optimizing the string returned

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Azure AKS!"
+    return ''.join([chr(ord(char) ^ 69) for char in "7$+e($7,&*+"])
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
